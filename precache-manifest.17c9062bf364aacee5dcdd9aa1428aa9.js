0f8559a447d901b26aa9fc98de908d2eb331c7f1
Priyabrat Mishra <priyabrat.misra@gmail.com>
2020-10-01 02:28:01 +0200

    Updates

---
+++ precache-manifest.17c9062bf364aacee5dcdd9aa1428aa9.js
@@ -0,0 +1,78 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ffa784575e68222dbb34c76b1dcbe1b5",
+    "url": "/home/index.html"
+  },
+  {
+    "revision": "d48214c2be3b5a1a26ac",
+    "url": "/home/static/css/2.5b44f0a0.chunk.css"
+  },
+  {
+    "revision": "35764752c32d78d841a0",
+    "url": "/home/static/css/main.6a3d9078.chunk.css"
+  },
+  {
+    "revision": "d48214c2be3b5a1a26ac",
+    "url": "/home/static/js/2.34b6cac1.chunk.js"
+  },
+  {
+    "revision": "0001512ff5376eafd7f76369861b7a6e",
+    "url": "/home/static/js/2.34b6cac1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "35764752c32d78d841a0",
+    "url": "/home/static/js/main.b4de2111.chunk.js"
+  },
+  {
+    "revision": "c0578b487099793a068c",
+    "url": "/home/static/js/runtime-main.3c590061.js"
+  },
+  {
+    "revision": "15cb58a8ea6eda8abc68d20fc8ee97bb",
+    "url": "/home/static/media/Profile.15cb58a8.jpg"
+  },
+  {
+    "revision": "e0beff4be3ed25e10ff0ac65a124e231",
+    "url": "/home/static/media/TechCV.e0beff4b.pdf"
+  },
+  {
+    "revision": "1096cc9af6a0b15c6fa3accce5195070",
+    "url": "/home/static/media/boeing.1096cc9a.jpg"
+  },
+  {
+    "revision": "241c3b8e66f1fc60ade69a3dba6e461b",
+    "url": "/home/static/media/committee.241c3b8e.jpg"
+  },
+  {
+    "revision": "518ff4419360e1c988e678996fa42e26",
+    "url": "/home/static/media/culture.518ff441.jpg"
+  },
+  {
+    "revision": "2d2b2b61779c56aa05843f899c5a9f04",
+    "url": "/home/static/media/custodians.2d2b2b61.jpg"
+  },
+  {
+    "revision": "fad82548bc64c3860539fda3da5aa944",
+    "url": "/home/static/media/dell-logo.fad82548.jpg"
+  },
+  {
+    "revision": "89c83c3b4dda7fed3a30ba8aa440bc8c",
+    "url": "/home/static/media/park.89c83c3b.jpg"
+  },
+  {
+    "revision": "d7e03d05cc830eda89d92c138ae9fc29",
+    "url": "/home/static/media/retreat.d7e03d05.jpg"
+  },
+  {
+    "revision": "0113f82a1a89a30897d85bce49f56dca",
+    "url": "/home/static/media/sec.0113f82a.jpg"
+  },
+  {
+    "revision": "afb11bbbfb026559897a133a1cdb8e4a",
+    "url": "/home/static/media/service.afb11bbb.jpg"
+  },
+  {
+    "revision": "d5cfdb857721fb2bc4c3049a34cb1ea1",
+    "url": "/home/static/media/zilker.d5cfdb85.jpg"
+  }
+]);
